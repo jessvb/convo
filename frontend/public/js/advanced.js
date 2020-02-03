89a580a4f032ce4c00f574d4a4ffe0f0aaff6cfc
Kevin Weng <kweng@mit.edu>
2020-02-02 23:25:40 -0500

    Fix advanced instructions

--- frontend/public/js/advanced.js
+++ frontend/public/js/advanced.js
@@ -22,9 +22,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             </div>
         </div>`;
 
-    initial_utter1 = "Great job! Did you notice that I only listened for user input once after you ran the procedure?"
+    initial_utter1 = "Great job in the novice stage! Did you notice that I only listened for user input once after you ran the procedure?"
     initial_utter2 = "This time, let’s add a countdown. I will continue to listen for user input until I’ve responded five times."
-    initial_utter3 = "Here is what the end result looks like."
     addUtter("agent-utter", initial_utter1, false);
     addUtter("agent-utter", initial_utter2, false);
     addUtter("agent-utter", initial_utter3, false);
