a308047e1d71dcc0cb6e59151a5e1d2f2324e56f
Kevin Weng <kweng@mit.edu>
2020-04-04 17:41:30 -0400

    add to gitignore

--- backend/server/app.py
+++ backend/server/app.py
@@ -6,7 +6,8 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/convo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/convo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 CORS(app)
 db = SQLAlchemy(app)
