1232f53236ae7408f9087bca372e5878d897d171
catyeo18 <catherineyeo2018@gmail.com>
2020-05-09 10:29:12 -0700

    Add q&a prediction into answering protocol

--- backend/server/question.py
+++ backend/server/question.py
@@ -1,4 +1,5 @@
 import re
+from bert import QA
 
 ask_what_procedures_regex = "what (.+)?procedures"
 ask_what_which_step_regex = "(?:what|which) step(?:.+)?|where am i"
@@ -33,6 +34,12 @@ class QuestionAnswer(object):
             else:
                 step_message = f"I am on step {edit.step + 1}"
             return f"{step_message} of the {edit.scope}, where I am {edit.current.to_nl()}."
+        else:
+            # Run BERT Q&A for any other question
+            model = QA("model")
+            context = "Convo is a voice-based system allowing users to develop computer programs by conversing in natural language with a conversational agent. The possible actions in this system are creating programs, editing programs, and receiving system feedback on how your command has impacted your program. The possible commands in this system are creating procedures, constructing loops, creating conditionals, declaring variables, and listening for user input. Convo uses Google's Cloud Speech-To-Text API to recognize voices and transcribe what a user says. Then, the transcribed output is sent to a WebSocket server where the natural language understanding and post-processing occur. Responses are voiced back to users using Google's Speech Synthesis API. Input phrases are mapped to output commands through a dialog manager, which tracks user goals and stores new states accordingly. Convo is still somewhat constrained in its understanding and setup, which may lead to mistakes or lacking features, but it will definitely improve over time! Our end goal is to develop a programming tool targeted for children to lower the barrier to entry to programming."
+            out = model.predict(context, question)
+            return jsonify({"result":out})["answer"]
 
     @staticmethod
     def is_question(message):
