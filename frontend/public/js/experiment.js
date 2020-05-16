6ca1ee5cf34860c8816c8009a7319f994fb93636
Helen Li <helen82198@gmail.com>
2020-05-15 23:07:34 -0700

    add help modals for each step of tutorial

--- frontend/public/js/experiment.js
+++ frontend/public/js/experiment.js
@@ -179,6 +179,9 @@ const example_commands = {
 const example_analysis_tutorial_commands = {
     "home": [{
             "title": "Create a Procedure or Program",
+            "help": `First, you will need to create a procedure. <br>
+            <b>CT Skill “Sequences</b>”: Keep in mind that as you are coding, things happen in order. So if your method has steps a, b, c, d, it will run in that order.
+            `,
             "examples": [
                 "create a procedure",
                 "make a procedure called AskForCompliment"
@@ -186,6 +189,9 @@ const example_analysis_tutorial_commands = {
         },
         {
             "title": "Get User Input to ask for a compliment and give it a name so we can analyze it later on",
+            "help": `Next, to interact with the user to see which phrase they want to analyze the toxicity of,
+            ask the user for their input. Then, store their input in a variable so that we can analyze it in the next step!
+            `,
             "examples": [
                 "get user input",
                 "get user input and call it userinput"
@@ -193,24 +199,45 @@ const example_analysis_tutorial_commands = {
         },
         {
             "title": "Analyze the Phrase - Note that analyze a phrase creates a variable <i>toxvar</i> to store the toxicity score",
+            "help": `
+            <b>CT Skill “data”:</b> 
+            Understand how machines learn from a ton of data. It might be interesting to introduce different machine models here and how data is trained / learned from
+            <br>Teach students about ethics of bias: how ML can be biased due to the dataset it was trained on
+            <br>Find examples of when the model incorrectly classifies something as toxic / not toxic
+            <br>Store the user input toxicity probability
+            <br>Introduce the ethics of AI model correctness. Is this model always correct?
+            `,
             "examples": [
                 "analyze the value of the variable userinput"
             ]
         },
         {
             "title": "Make a While Loop",
+            "help": `
+            <b>CT Skill “conditionals”:</b> 
+            Our computer needs out if a sentence is toxic and what to do after that
+            <br >Introduce the ethics of a “cutoff score” 
+            <br> When should something be classified as toxic? Should it be above 50%? Should we be more careful and classify things as toxic more often? Or should we classify things as toxic less often? (false negatives vs false positives)
+            <br> Does it differ based on the application you’re creating?
+            `,
             "examples": [
                 "make a while loop"
             ]
         },
         {
             "title": "Give while loop conditions",
+            "help": `
+            <i>While</i> the probability is high, then ask the user to enter something else that’s nicer
+            `,
             "examples": [
                 "while toxvar is greater than 0.5",
             ]
         },
         {
             "title": "Give next steps in loop conditions",
+            "help": `
+            <i>While</i> the probability is high, then ask the user to enter something else that’s nicer
+            `,
             "examples": [
                 "say 'Your phrase is not nice enough. Enter a new one.'",
                 "get user input and call it userinput",
@@ -219,12 +246,17 @@ const example_analysis_tutorial_commands = {
         },
         {
             "title": "Close the loop",
+            "help": `We are done with the loop, so close it now! We have outlined everything that will happen
+            while the toxicity score is high."
+            `,
             "examples": [
                 "close loop",
             ]
         },
         {
             "title": "Make Me Say Something",
+            "help" : `Have the program let the user know what the compliment was! After all, we are asking for compliments.
+            `,
             "examples": [
                 "say 'Here is your compliment!'",
                 "say the value of the variable userinput",
@@ -234,6 +266,8 @@ const example_analysis_tutorial_commands = {
         },
         {
             "title": "Run a Procedure or Program",
+            "help": `You must run a program before anything can execute/happen.
+            `,
             "examples": [
                 "run AskForCompliment"
             ]
@@ -324,15 +358,16 @@ let handleAnalysisTutorialChange = (newState) => {
     examples.innerHTML = "";
     if (state != null && state in example_analysis_tutorial_commands) {
         let commands = example_analysis_tutorial_commands[state];
-        commands.forEach(action => {
+        for (var i = 0; i < commands.length; i++) {
+            action = commands[i];
             let node = document.createElement('div');
             node.className = "example-analysis-tutorial-action"
-            node.innerHTML = `<div class="action-title"><b>${action.title}</b></div>`;
-            action.examples.forEach(ex => {
-                node.innerHTML += `<div class="action-example"><em>${ex}</em></div>`;
-            })
+            node.innerHTML = `<div class="action-title"><b>${action.title}</b>   <button class="button-help" id="button-help-` + i + `">Help</button></div>`;
+            // action.examples.forEach(ex => {
+            //     node.innerHTML += `<div class="action-example"><em>${ex}</em></div>`;
+            // })
             examples.appendChild(node);
-        })
+        }
     } 
 }
 
@@ -524,22 +559,42 @@ let populateModalInitial = () => {
 
         </li>
     </ol>
+    <div class="modal-content-button">
+        <button id="modal-next">Next</button>
+    </div>
 `
 }
 
 let populateModalNext = () => { 
     document.getElementById("modal-content-paragraph").innerHTML = `
-    In this tutorial, you will be learning that a machine learning model takes in a sentence you write and outputs how “toxic” the sentence is on a scale of 0 to 1. We are aiming to have the user type in a sentence with a low toxicity rate to be nice to each other! 
+    In this tutorial, you will be learning that using an API, a machine learning model takes in a sentence you write and outputs how “toxic” the sentence is on a scale of 0 to 1.
+    We are aiming to have the user type in a sentence with a low toxicity rate to be nice to each other! 
         <ol>
-            <li>First, we will be using an API. <b>What is an API?<b>
+            <li>First, we will be using an API. <b>What is an API?</b><br>
+            API (application program interface) is a set of components with routines, protocols, and tools
+            that allows the user to build software! We will be making requests to <b>Google's Toxicity API</b>, 
+            and that will allow us to evaluate how "toxic" a phrase is on a scale from 0 to 1. 
+            <a href="https://www.perspectiveapi.com/#/home">More info here</a>
                 
             </li>
-            <li><b>What is machine learning?</b>
-
+            <li><b>What is machine learning?</b><br>
+            Machine learning is when you give lots of data to a computer program and choose a model to “fit” the data, 
+            which allows the computer to come up with predictions. The computer uses algorithms to learn patterns, which can range from 
+            a simple equation (like the equation of a line) to a very complex system of logic/math results in the best prediction.
+            <br><br>
+            In this tutorial, Google's Perspective API uses machine learning by analyzing a ton of sentences and learning how "toxic" those sentences are.
+            Then, when a user sends a request to request a toxicity score for a random phrase, the Perspective machine learning model
+            will be able to return how toxic the phrase is.
+            <br><br>
+            Read more about an simple definition of machine learning <a href="https://thenextweb.com/neural/2020/04/25/machine-learning-models-explained-to-a-five-year-old-syndication/">here</a>
             </li>
         </ol>
     To begin, we’ll guide you through the tutorial to write your own Convo code!
+    <div class="modal-content-button">
+        <button id="modal-close">Close</button>
+    </div>
     `
+    
 }
 
 let populateAndDisplayModal = () => {
@@ -560,6 +615,10 @@ let populateAndDisplayModal = () => {
     var nextButton = document.getElementById("modal-next");
     nextButton.onclick = function() {
         populateModalNext();
+        var closeButton = document.getElementById("modal-close");
+        closeButton.onclick = function() {
+            modal.style.display = "none";
+        }
     }
 
     // When the user clicks anywhere outside of the modal, close it
@@ -570,6 +629,45 @@ let populateAndDisplayModal = () => {
     }   
 }
 
+let populateHelpModal = () => {
+    var allHelpButtons = document.getElementsByClassName("button-help");
+    for(var i=0; i<allHelpButtons.length; i++) {
+        let stepButton = allHelpButtons[i]
+        console.log(stepButton.id);
+        stepButton.onclick = function() {
+            let stepButtonIdArray = stepButton.id.split("-");
+            console.log(stepButtonIdArray);
+            let buttonId = parseInt(stepButtonIdArray[stepButtonIdArray.length-1]) // get the ID # 
+            populateHelpModalWithInfo(buttonId)
+        }
+    };
+}
+
+let populateHelpModalWithInfo = (i) => {
+    let commandForStep = example_analysis_tutorial_commands[state][i];
+    let commandTitle = commandForStep.title;
+    let commandHelp = commandForStep.help;
+    let commandExamples = commandForStep.examples;
+    var modalContentHelpParagraph = document.getElementById("modal-content-paragraph");
+    modalContentHelpParagraph.innerHTML = "";
+    modalContentHelpParagraph.innerHTML += "<div><b>"+commandTitle+"</b></div>";
+    modalContentHelpParagraph.innerHTML += "<ol>"
+    commandExamples.forEach((command) => {
+        modalContentHelpParagraph.innerHTML += "<li><i>"+command+"</i></</li>";
+    });
+    modalContentHelpParagraph.innerHTML += "</ol>"
+    modalContentHelpParagraph.innerHTML += "<br><div>"+commandHelp+"</div>";
+
+    modalContentHelpParagraph.innerHTML += `<div class="modal-content-button">
+        <button id="modal-close">Close</button>
+    </div>
+    `
+    var modal = document.getElementById("myModal");
+
+    // Open the modal 
+    modal.style.display = "block";
+
+}
 document.addEventListener("DOMContentLoaded", () => {
     let textbox = document.getElementById("textbox");
     if (textbox != null) {
@@ -596,6 +694,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     populateAndDisplayModal();
+    populateHelpModal();
  
     addAudioPlayer();
 });
