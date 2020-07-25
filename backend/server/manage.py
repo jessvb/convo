5d2deb61a540e8f4fa66947ab639f972a0ad08bb
fyang3 <42852118+fyang3@users.noreply.github.com>
2020-07-25 12:15:43 -0400

    fix the intro phrase

--- backend/server/manage.py
+++ backend/server/manage.py
@@ -64,7 +64,7 @@ def join(data):
     intro = '''
         Hi, I'm Convo! What would you like to do?
         To get started, you can create a procedure by saying "Create a procedure".
-        If you want to run a procedure, say "Run" and then the name of the procedure.
+        If you want to run a procedure, say "Run" and the name of the procedure.
     '''
     response = {
         "message": intro,
