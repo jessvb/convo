f0f35e85f4090f26ac5199cc69adcb3e3b058db9
Jessica Zhu <jfzhu@mit.edu>
2020-10-13 15:05:22 -0700

    fix rasa server connection issues

--- backend/server/rasa_nlu.py
+++ backend/server/rasa_nlu.py
@@ -36,12 +36,13 @@ class RasaNLU(object):
         self.confidence_threshold = confidence_threshold
 
     def parse_message(self, message):
+        logger.debug("Rasa parsing message")
         payload = json.dumps({"text": message.lower()})
         res = None
 
         try:
-            res = requests.post("http://localhost:5005/model/parse", data=payload)
-        except requests.ConnectionError:
+            res = requests.post("http://rasa:5005/model/parse", data=payload)
+        except requests.ConnectionError as e:
             logger.info("Cannot connect to Rasa server.")
             return None
 
@@ -54,9 +55,11 @@ class RasaNLU(object):
 
         if intent["confidence"] < self.confidence_threshold:
             # If confidence is less than threshold, do not use intent
+            logger.debug("confidence is less than threshold")
             return None
         if intent["name"] not in intent_goal:
             # If intent is not supported currently by the NLU
+            logger.debug("intent not supported by NLU")
             return None
 
         goal = intent_goal[intent["name"]]
