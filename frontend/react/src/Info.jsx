1f7c48e1908a65508867a7a80457adf98ee26734
Jessica Zhu <jessicazhu2006@gmail.com>
2021-02-11 19:31:56 -0800

    remove unnecessary jumbotron component

--- frontend/react/src/Info.jsx
+++ frontend/react/src/Info.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Jumbotron } from './components/Jumbotron';
 
 export const Info = () => (
     <div>
-        <Jumbotron />
         <h2>Program by Speaking!</h2>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis, nisl ut aliquet congue, odio sem semper augue, sed auctor magna nibh id magna. Maecenas eu magna sit amet nisi ornare tincidunt ac ultrices purus. Suspendisse ac ex a enim venenatis mattis sit amet at sapien. Morbi vitae augue ipsum. Ut pharetra convallis ipsum, ac rhoncus felis pharetra vitae. Proin condimentum sollicitudin elit et iaculis. Phasellus sagittis finibus sem non viverra. In purus turpis, rutrum pulvinar fermentum ut, faucibus aliquam turpis. Etiam fermentum euismod massa. Cras ut justo sapien. In hac habitasse platea dictumst. Vivamus pulvinar lorem accumsan, euismod nibh et, ullamcorper turpis. Nullam semper dolor in suscipit congue. Aliquam consequat lorem a turpis lacinia, fermentum commodo tellus ornare.
